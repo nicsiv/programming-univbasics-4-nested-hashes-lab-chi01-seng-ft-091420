e3c71bbc35d28321167ca0f8cb25d26c89651a15
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-09-02 00:14:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -114,7 +114,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-puts changing_alan[:alan_kay][:known_for] => "GUI"
+puts changing_alan[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
